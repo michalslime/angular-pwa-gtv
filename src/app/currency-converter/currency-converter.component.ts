311fad7b6820cd0544b7905f78ba59b020fc6e31
Michał Strzoda <michal.strzoda@gmail.com>
2024-01-23 08:07:22 +0100

    Task #2 - value is converted upon selecting currency from drop down

--- src/app/currency-converter/currency-converter.component.ts
+++ src/app/currency-converter/currency-converter.component.ts
@@ -2,34 +2,48 @@ import { Component, OnInit } from '@angular/core';
 import { NbpService, Currency } from '../services/nbp.service';
 
 @Component({
-  selector: 'app-currency-converter',
-  template: `
+    selector: 'app-currency-converter',
+    template: `
     <div>
       <input type="number" [(ngModel)]="amount" placeholder="Enter amount">
-      <select [(ngModel)]="selectedCurrency"  (change)="onCurrencySelect($event)">
-        <option *ngFor="let currency of currencies" [value]="currency">
+      <select [(ngModel)]="selectedCurrency" (ngModelChange)="onCurrencySelect()">
+        <option *ngFor="let currency of currencies" [value]="currency.code">
           {{ currency.code }}
         </option>
       </select>
-      <span *ngIf="calculatedValue !== null">Result: {{ calculatedValue }}</span>
+      <div *ngIf="exchangeRate === null">There is no exchange rate for today yet, or today is bank holiday</div>
+      <div *ngIf="exchangeRate !== null">Exchange Rate: {{ exchangeRate }}</div>
+      <div *ngIf="calculatedValue !== null">Result: {{ calculatedValue }}</div>
     </div>
   `
 })
 export class CurrencyConverterComponent implements OnInit {
-  amount: number = 0;
-  selectedCurrency!: Currency;
-  calculatedValue: number | null = null;
-  currencies: Currency[] = [];
+    amount: number = 0;
+    selectedCurrency: string = '';
+    exchangeRate!: number | null;
+    calculatedValue: number | null = null;
+    currencies: Currency[] = [];
 
-  constructor(private NbpService: NbpService) { }
+    constructor(private NbpService: NbpService) { }
 
-  ngOnInit() {
-    this.NbpService.getCurrencies().subscribe(data => {
-      this.currencies = data;
-    });
-  }
+    ngOnInit() {
+        this.NbpService.getCurrencies().subscribe(data => {
+            this.currencies = data;
+        });
+    }
 
-  onCurrencySelect(e: any) {
-    this.calculatedValue = this.amount * 2;
-  }
+    onCurrencySelect() {
+        if (this.selectedCurrency) {
+            const today = new Date().toISOString().split('T')[0];
+            this.NbpService.getExchangeRate(this.selectedCurrency, today)
+                .subscribe((rate: number | null) => {
+                    this.exchangeRate = rate;
+                    if (this.exchangeRate) {
+                        this.calculatedValue = this.amount * this.exchangeRate;
+                    } else {
+                        this.calculatedValue = 0;
+                    }
+                });
+        }
+    }
 }
--- src/app/services/nbp.service.ts
+++ src/app/services/nbp.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 export interface Currency {
     code: string;
@@ -11,13 +11,28 @@ export interface Currency {
     providedIn: 'root'
 })
 export class NbpService {
-    private apiUrl = 'http://api.nbp.pl/api/exchangerates/tables/A/?format=json';
+    private baseUrl = 'https://api.nbp.pl/api/exchangerates';
 
     constructor(private http: HttpClient) { }
 
     public getCurrencies(): Observable<Currency[]> {
-        return this.http.get<any[]>(this.apiUrl).pipe(
+        const url = `${this.baseUrl}/tables/A/?format=json`
+        return this.http.get<any[]>(url).pipe(
             map(response => response[0].rates.map((data: { code: any; }) => ({ code: data.code })))
         );
+    }
+
+    public getExchangeRate(code: string, date: string): Observable<any> {
+        const url = `${this.baseUrl}/rates/A/${code}/${date}/?format=json`;
+        return this.http.get<any>(url).pipe(
+            map(response => {
+                return response.rates && response.rates.length > 0 ? response.rates[0].mid : null;
+            }), 
+            catchError((error: HttpErrorResponse) => {
+
+              console.error('Error fetching exchange rate:', error);
+              return of(null);
+            })
+          );
     }
 }
