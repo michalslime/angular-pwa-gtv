e128245491ebb64834dd7e7a7dacd0e154a9d12b
Michał Strzoda <michal.strzoda@gmail.com>
2024-01-23 07:05:59 +0100

    Task #1 - Component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImageSwitcherComponent } from './image-switcher/image-switcher.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'image-switcher', component: ImageSwitcherComponent }
+];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes),
-  ],
-  exports: [
-    RouterModule,
-  ],
+    imports: [
+        RouterModule.forRoot(routes),
+    ],
+    exports: [
+        RouterModule,
+    ],
 })
 export class AppRoutingModule {
 }
---
+++ src/app/image-switcher/image-switcher.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ImageService } from '../image.service';
+import { Subscription, interval } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+@Component({
+    selector: 'app-image-fetcher',
+    template: `<img *ngIf="imageUrl" [src]="imageUrl" alt="Random Image">`
+})
+export class ImageSwitcherComponent implements OnInit, OnDestroy {
+    imageUrl!: string;
+    private fetchSubscription!: Subscription;
+
+    constructor(private imageService: ImageService) { }
+
+    ngOnInit() {
+        this.fetchSubscription = interval(30000)
+            .pipe(
+                switchMap(() => this.imageService.getImageUrl())
+            )
+            .subscribe(data => {
+                this.imageUrl = data;
+            });
+    }
+
+    ngOnDestroy() {
+        if (this.fetchSubscription) {
+            this.fetchSubscription.unsubscribe();
+        }
+    }
+}
