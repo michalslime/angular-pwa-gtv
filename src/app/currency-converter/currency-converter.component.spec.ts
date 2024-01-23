827c3fc6e4d0e2f598530094ec43949d82a5b02c
Michał Strzoda <michal.strzoda@gmail.com>
2024-01-23 08:18:00 +0100

    Task #2 - CurrencyConverter component Unit Tests

--- src/app/currency-converter/currency-converter.component.spec.ts
+++ src/app/currency-converter/currency-converter.component.spec.ts
@@ -1,23 +1,51 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CurrencyConverterComponent } from './currency-converter.component';
+import { FormsModule } from '@angular/forms';
+import { NbpService } from '../services/nbp.service';
+import { of } from 'rxjs';
 
 describe('CurrencyConverterComponent', () => {
-  let component: CurrencyConverterComponent;
-  let fixture: ComponentFixture<CurrencyConverterComponent>;
+    let component: CurrencyConverterComponent;
+    let fixture: ComponentFixture<CurrencyConverterComponent>;
+    let nbpService: NbpService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CurrencyConverterComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        const nbpServiceMock = {
+            getCurrencies: () => of([{ code: 'USD' }, { code: 'EUR' }]),
+            getExchangeRate: () => of(2)
+        };
 
-    fixture = TestBed.createComponent(CurrencyConverterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        await TestBed.configureTestingModule({
+            imports: [FormsModule],
+            declarations: [CurrencyConverterComponent],
+            providers: [{ provide: NbpService, useValue: nbpServiceMock }]
+        })
+            .compileComponents();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CurrencyConverterComponent);
+        component = fixture.componentInstance;
+        nbpService = TestBed.inject(NbpService);
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should populate currencies on init', () => {
+        component.ngOnInit();
+        expect(component.currencies.length).toBeGreaterThan(0);
+    });
+
+    it('should call getExchangeRate and calculate result when a currency is selected', () => {
+        const exchangeRateSpy = spyOn(nbpService, 'getExchangeRate').and.returnValue(of(2));
+
+        component.selectedCurrency = 'USD';
+        component.onCurrencySelect();
+
+        expect(exchangeRateSpy).toHaveBeenCalledWith('USD', jasmine.any(String));
+        expect(component.exchangeRate).toBe(2);
+    });
 });
