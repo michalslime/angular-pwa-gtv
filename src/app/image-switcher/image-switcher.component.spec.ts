405ae4f13c01cf0436450b9a7157d27b82b13af9
Michał Strzoda <michal.strzoda@gmail.com>
2024-01-23 07:13:53 +0100

    Task #1 - Unit tests for components

--- src/app/image-switcher/image-switcher.component.spec.ts
+++ src/app/image-switcher/image-switcher.component.spec.ts
@@ -1,23 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ImageService } from '../image.service';
+import { Observable, Subscription, of } from 'rxjs';
 import { ImageSwitcherComponent } from './image-switcher.component';
 
-describe('ImageSwitcherComponent', () => {
-  let component: ImageSwitcherComponent;
-  let fixture: ComponentFixture<ImageSwitcherComponent>;
+describe('ImageFetcherComponent', () => {
+    let component: ImageSwitcherComponent;
+    let fixture: ComponentFixture<ImageSwitcherComponent>;
+    let mockImageService: { getImageUrl: { and: { returnValue: (arg0: Observable<{ url: string; }>) => void; }; }; };
+    let mockResponse = { url: 'https://example.com/image.jpg' };
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ImageSwitcherComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        // Mock ImageService
+        mockImageService = jasmine.createSpyObj(['getImageUrl']);
 
-    fixture = TestBed.createComponent(ImageSwitcherComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        await TestBed.configureTestingModule({
+            declarations: [ImageSwitcherComponent],
+            // Provide the mock instead of the actual service
+            providers: [
+                { provide: ImageService, useValue: mockImageService }
+            ]
+        })
+            .compileComponents();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ImageSwitcherComponent);
+        component = fixture.componentInstance;
+        // Mock the service call
+        mockImageService.getImageUrl.and.returnValue(of(mockResponse));
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should unsubscribe on destroy', () => {
+        const fetchSubscription = component['fetchSubscription'] as Subscription;
+
+        spyOn(fetchSubscription, 'unsubscribe');
+        component.ngOnDestroy();
+        expect(fetchSubscription.unsubscribe).toHaveBeenCalled();
+    });
 });
